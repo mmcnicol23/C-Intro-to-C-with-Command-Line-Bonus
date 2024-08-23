3939f5db3db2d9858bc6621a228fafc0f2eb09cf
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-23 15:45:15 -0400

    completed hello world exercise

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Howdy World! How are you doing?");
+string userinput = Console.ReadLine();
+Console.WriteLine($"Glad to hear that you're {userinput}!");
